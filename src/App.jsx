bab571d214afbeba7bee7177a88515b2ccf45a2c
Louis <louis.dubois@epitech.eu>
2026-09-23 23:51:38 +0200

    ADD: get pikachu image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 export default function App() {
+    async function getPokemonImage(id) {
+        try {
+            const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+
+            if (!pokemon.ok)
+                throw new Error("Error HTTP: ", pokemon.status);
+
+            const pokemonData = await pokemon.json();
+            const imageUrl = pokemonData.sprites.font_default;
+
+            if (imageUrl)
+                return imageUrl;
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    }
+ 
     return (
         <>
         </>
