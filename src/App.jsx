b4726f86cd7c92f55fd3e332d556bbf8f3fa42df
Louis <louis.dubois@epitech.eu>
2026-09-24 13:51:55 +0200

    ADD: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getPokemonImageById } from "./api/getPokemonImageById";
+import Header from "./components/header";
 
 export default function App() {
     const ids = [1, 4, 7, 10, 16, 19, 25, 77, 130, 133, 143, 150];
@@ -7,20 +8,40 @@ export default function App() {
 
     useEffect(() => {
         let cancelled = false;
-
-        Promise.all(ids.map(id => getPokemonImageById(id)))
-            .then(urls => {
-                if (!cancelled) setImages(urls);
-            });
+        const loadImages = async () => {
+            try {
+                const urls = await Promise.all(ids.map(id => getPokemonImageById(id)));
+                if (!cancelled) {
+                    const imgs = urls.map((url, id) =>({
+                        id: id,
+                        url: url 
+                    }));
+                    setImages(imgs);
+                }
+            } catch (error) {
+                console.error("Error: ", error);
+                if (!cancelled) {
+                    setImages([]); 
+                }
+            }
+        };
+        loadImages();
 
         return () => { cancelled = true; };
     }, []);
  
     return (
         <>
-            {images.map((image, index) => (
-                <img key={ids[index]} src={image} alt="Pokemon" />
-            ))}
+            <div>
+                <Header />
+            </div>
+            <div>
+                <div className="w-[600px] flex flex-wrap border rounded-[5px] justify-center">
+                    {images.map(image => (
+                        <img key={image.id} src={image.url} alt="Pokemon" />
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import Logo from '../assets/logo.png';
+
+export default function Header() {
+    return (
+        <div className="h-16 bg-blue-950 text-white font-bold flex justify-between items-center pl-4 pr-4">
+            <div className="flex items-center gap-2">
+                <img
+                    src={Logo}
+                    alt="Logo"
+                    className="h-8 rounded-[25px]"
+                />
+                <h1>PokéMory</h1>
+            </div>
+            <button className="bg-blue-200 text-black p-1 rounded-[5px] border border-black hover:scale-110 active:scale-95 transition-all ease-out">
+                New Game
+            </button>
+            <div className="flex justify-center gap-8">
+                <span>Score : 0</span>
+                <span>Record : 0</span>
+            </div>
+        </div>
+    );
+}
